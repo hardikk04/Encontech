a663be0368ca4f25bb0f7303639cbb15610eb450
hardikk04 <hardik04062003@gmail.com>
2023-12-13 00:37:54 +0530

    till page 5 done

--- script.js
+++ script.js
@@ -84,3 +84,14 @@ page2H3.forEach((h1, index) => {
     }
   });
 });
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+    centeredSlides: true,
+  spaceBetween: 30,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
